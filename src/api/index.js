37185274591aad0609e756948711581ae4785b14
王树杰 <9732462+wsjdjb@user.noreply.gitee.com>
2023-05-06 23:57:57 +0800

    删除全部等待新增

--- src/api/index.js
+++
@@ -1,8 +0,0 @@
-
-import requests from "./requests";
-import mockRequests from "./mockRequests"
-
-export const reqGetCategoryList = ()=>requests({url:"/product/getBaseCategoryList",method:"get"})
-
-export const  reqGetBannerList= ()=>mockRequests({url:"/banner",method:"get"})
-export const  reqGetFloorList= ()=>mockRequests({url:"/floor",method:"get"})
